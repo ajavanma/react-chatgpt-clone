1f518b807c1d6fad2ef33716391ad2301f2b67ea
Arash Javanmardi <ajavanma@rhrk.uni-kl.de>
2023-04-30 18:17:31 +0200

    KSKHL

--- server.js
+++ server.js
@@ -6,4 +6,28 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 
+const API_KEY = 'API_KEY'
+
+app.post('/completions', async (req, res) => {
+    const options = {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Bearer ' + API_KEY,
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            model: "gpt-3.5-turbo",
+            messages: [{role: "user", content: "string for now"}],
+            max_tokens: 150,
+            })
+        }
+
+    try {
+        const response = await fetch('https://api.openai.com/v1/chat/completions', options)
+
+    } catch(err) {
+        console.log(err)
+    }
+})
+
 app.listen(PORT, () => console.log('Server running on port port: ' + PORT))
