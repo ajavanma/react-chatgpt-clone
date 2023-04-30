e856e173e71c134189d2caa8916827ec7ad0ae99
Arash Javanmardi <ajavanma@rhrk.uni-kl.de>
2023-04-30 12:47:32 +0200

    usPVD

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 const App = () => {
   return (
     <div className="app">
-      
+      <section className="side-bar">
+        <button>+ New chat</button>
+        <ul className="history"></ul>
+        <nav>
+          <p>Made by Arash</p>
+        </nav>
+      </section>
+
+      <section className="main"> </section>
     </div>
   )
 }
